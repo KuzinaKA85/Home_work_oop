085bf2c0ceb11dbc8fb58db7d922c04d50feb782
Kseniya Kuzina <kolosova_ka@mail.ru>
2025-10-04 14:06:46 +0300

    init class Product, Category. Init .flake8. Init main.

---
+++ src/product.py
@@ -0,0 +1,15 @@
+class Product:
+    """Класс, описывающий свойства продукта"""
+
+    name: str
+    description: str
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        """Метод для инициализации экземпляра класса. Задаем значения атрибутам экземпляра"""
+
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
