bb5ed559a08b9a6c1a771896413423fd0c534bd0
Kseniya Kuzina <kolosova_ka@mail.ru>
2025-10-23 20:13:01 +0300

    init print_mixin.py, update product.py

--- src/product.py
+++ src/product.py
@@ -1,7 +1,8 @@
 from src.baseproduct import BaseProduct
+from src.print_mixin import PrintMixin
 
 
-class Product(BaseProduct):
+class Product(BaseProduct, PrintMixin):
     """Класс, описывающий свойства продукта"""
 
     name: str
@@ -16,6 +17,8 @@ class Product(BaseProduct):
         self.description = description
         self.__price = price
         self.quantity = quantity
+        super().__init__()
+
 
     def __str__(self):
         return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
